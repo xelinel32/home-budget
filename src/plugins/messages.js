606fb267c509ee15940d0d4037978aeeee7841d9
xelinel32 <artemsedlyar99@gmail.com>
2020-06-28 18:16:38 +0300

    route defence

--- src/plugins/messages.js
+++ src/plugins/messages.js
@@ -2,5 +2,6 @@ export default {
   'logout': 'Вы вышли из системы',
   'auth/user-not-found': 'Пользователь не найден',
   'auth/wrong-password': 'Неверный пароль',
-  'auth/email-already-in-use': 'Email уже занят'
+  'auth/email-already-in-use': 'Email уже занят',
+  'login': 'Нужно сначала войти' 
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import fb from 'firebase/app';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -22,44 +23,57 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/Home.vue'),
     },
     {
       path: '/categories',
       name: 'categories',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/Categories.vue'),
     },
     {
       path: '/detail/:id',
       name: 'detail',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/Detail.vue'),
     },
     {
       path: '/history',
       name: 'history',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/History.vue'),
     },
     {
       path: '/planning',
       name: 'planning',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/Planning.vue'),
     },
     {
       path: '/profile',
       name: 'profile',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/Profile.vue'),
     },
     {
       path: '/record',
       name: 'record',
-      meta: { layout: 'main' },
+      meta: { layout: 'main', auth: true },
       component: () => import('../views/Record.vue'),
     },
   ],
+});
+
+export default router;
+
+router.beforeEach((to, from, next) => {
+  const currntUser = fb.auth().currentUser;
+  const requireAuth = to.matched.some((rec) => rec.meta.auth);
+
+  if (requireAuth && !currntUser) {
+    next('/login?message=login');
+  } else {
+    next();
+  }
 });
