558b5da40eab738da3240f2e39999c3dd17a50cf
xelinel32 <artemsedlyar99@gmail.com>
2020-06-28 18:39:01 +0300

    create history logic

---
+++ src/components/v-history-table.vue
@@ -0,0 +1,55 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Сумма</th>
+        <th>Дата</th>
+        <th>Категория</th>
+        <th>Тип</th>
+        <th>Открыть</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(record, i) in records" :key="record.id">
+        <td>{{ i }}</td>
+        <td>{{ record.amount | currency('RUB') }}</td>
+        <td>{{ record.data | dateFilter }}</td>
+        <td>{{ record.categoryName }}</td>
+        <td>
+          <span class="white-text badge" :class="[record.typeClass]">{{
+            record.typeText
+          }}</span>
+        </td>
+        <td>
+          <button
+            class="btn-small btn"
+            v-tooltip="'Посмотреть'"
+            @click="$router.push('/detail/' + record.id)"
+          >
+            <i class="material-icons">open_in_new</i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import currency from '@/filters/currency';
+import dateFilter from '@/filters/dateFilter';
+export default {
+  name: 'v-history-table',
+  props: {
+    records: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  filters: { currency, dateFilter },
+};
+</script>
+
+<style></style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -7,37 +7,43 @@
     <div class="history-chart">
       <canvas></canvas>
     </div>
-
+    <Loader v-if="loading" />
+    <p v-else-if="!records.length" class="center">
+      Записей пока что нет
+      <router-link to="/record">Добавить новую запись</router-link>
+    </p>
     <section>
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Сумма</th>
-          <th>Дата</th>
-          <th>Категория</th>
-          <th>Тип</th>
-          <th>Открыть</th>
-        </tr>
-        </thead>
-
-        <tbody>
-        <tr>
-          <td>1</td>
-          <td>1212</td>
-          <td>12.12.32</td>
-          <td>name</td>
-          <td>
-            <span class="white-text badge red">Расход</span>
-          </td>
-          <td>
-            <button class="btn-small btn">
-              <i class="material-icons">open_in_new</i>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+      <vHistoryTable :records="records" />
     </section>
   </div>
 </template>
+
+<script>
+import vHistoryTable from '@/components/v-history-table';
+export default {
+  name: 'history',
+  data: () => ({
+    loading: true,
+    records: [],
+    categories: [],
+  }),
+  async mounted() {
+    // this.records = await this.$store.dispatch('FETCH_RECORDS');
+    const records = await this.$store.dispatch('FETCH_RECORDS');
+    this.categories = await this.$store.dispatch('FETCH_CATEGORIES');
+    this.records = records.map((record) => {
+      return {
+        ...record,
+        categoryName: this.categories.find((c) => c.id === record.categoryId)
+          .title,
+        typeClass: record.type === 'income' ? 'green' : 'red',
+        typeText: record.type === 'income' ? 'Доход' : 'Расход',
+      };
+    });
+    this.loading = false;
+  },
+  components: {
+    vHistoryTable,
+  },
+};
+</script>
