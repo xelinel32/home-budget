cb1481f1b9bd58964fb1cab6e7da6036fe17fb46
xelinel32 <artemsedlyar99@gmail.com>
2020-06-29 21:47:36 +0300

    localize app

--- src/router/index.js
+++ src/router/index.js
@@ -11,65 +11,66 @@ const router = new Router({
     {
       path: '/login',
       name: 'login',
-      meta: { layout: 'empty' },
-      component: () => import('../views/Login.vue'),
+      meta: { layout: 'empty', title: 'Логин' },
+      component: () => import('../views/Login.vue')
     },
     {
       path: '/register',
       name: 'register',
-      meta: { layout: 'empty' },
-      component: () => import('../views/Register.vue'),
+      meta: { layout: 'empty', title: 'Регистрация' },
+      component: () => import('../views/Register.vue')
     },
     {
       path: '/',
       name: 'home',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/Home.vue'),
+      meta: { layout: 'main', auth: true, title: 'Домашняя страница' },
+      component: () => import('../views/Home.vue')
     },
     {
       path: '/categories',
       name: 'categories',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/Categories.vue'),
+      meta: { layout: 'main', auth: true, title: 'Категории' },
+      component: () => import('../views/Categories.vue')
     },
     {
       path: '/detail/:id',
       name: 'detail',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/Detail.vue'),
+      meta: { layout: 'main', auth: true, title: 'Подробно' },
+      component: () => import('../views/Detail.vue')
     },
     {
       path: '/history',
       name: 'history',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/History.vue'),
+      meta: { layout: 'main', auth: true, title: 'История' },
+      component: () => import('../views/History.vue')
     },
     {
       path: '/planning',
       name: 'planning',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/Planning.vue'),
+      meta: { layout: 'main', auth: true, title: 'Планирование' },
+      component: () => import('../views/Planning.vue')
     },
     {
       path: '/profile',
       name: 'profile',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/Profile.vue'),
+      meta: { layout: 'main', auth: true, title: 'Профиль' },
+      component: () => import('../views/Profile.vue')
     },
     {
       path: '/record',
       name: 'record',
-      meta: { layout: 'main', auth: true },
-      component: () => import('../views/Record.vue'),
-    },
-  ],
+      meta: { layout: 'main', auth: true, title: 'Запись' },
+      component: () => import('../views/Record.vue')
+    }
+  ]
 });
 
 export default router;
 
 router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
   const currntUser = fb.auth().currentUser;
-  const requireAuth = to.matched.some((rec) => rec.meta.auth);
+  const requireAuth = to.meta.auth;
 
   if (requireAuth && !currntUser) {
     next('/login?message=login');
